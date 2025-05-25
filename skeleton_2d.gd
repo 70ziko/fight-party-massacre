6a8e622c12993dcab091bb62add0c86bacf0129b
70ziko <fidler.patryk@gmail.com>
2025-05-26 00:25:01 +0200

    feat: implement physical bones for stickman and enhance collision detection

---
+++ skeleton_2d.gd
@@ -0,0 +1,1 @@
+extends Skeleton2D
--- stickman_character_body_2d.gd
+++ stickman_character_body_2d.gd
@@ -1,7 +1,28 @@
 extends RigidBody2D
 
 func _ready():
-	# Enable Godot's built-in bone physics (ragdoll)
 	var skel = $Skeleton2D
+	print("Godot version:", Engine.get_version_info())
+	# print("Skeleton2D methods:", skel.get_method_list())
+
+	# Try to enable physical bones if possible
 	if skel.has_method("set_physical_bones_active"):
 		skel.set_physical_bones_active(true)
+		print("Physical bones activated for skeleton: ", skel)
+	else:
+		print("Skeleton2D does not support physical bones!")
+	
+	# skel.physical_bone_chain_length(2)
+
+	# Recursively print all PhysicalBone2D nodes and their CollisionShape2D children
+	_recursive_print_bones(skel)
+
+
+func _recursive_print_bones(node):
+	if node is PhysicalBone2D:
+		print("Found PhysicalBone2D: ", node.get_path())
+		for child in node.get_children():
+			if child is CollisionShape2D:
+				print("  Has CollisionShape2D: ", child.shape)
+	for child in node.get_children():
+		_recursive_print_bones(child)
