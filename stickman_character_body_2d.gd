10ef220b063b275034459898bf2432c6a7f24dca
70ziko <fidler.patryk@gmail.com>
2025-05-30 15:56:57 +0200

    feat: streamline gravity handling and clean up unused code in stickman_character_body_2d

--- stickman_character_body_2d.gd
+++ stickman_character_body_2d.gd
@@ -1,22 +1,20 @@
 extends CharacterBody2D
 
+var gravity: float = float(ProjectSettings.get_setting("physics/2d/default_gravity"))
+
 func _ready():
 	var skel = $Skeleton2D
 	print("Godot version:", Engine.get_version_info())
-	# print("Skeleton2D methods:", skel.get_method_list())
-
-	# Try to enable physical bones if possible
 	if skel.has_method("set_physical_bones_active"):
 		skel.set_physical_bones_active(true)
 		print("Physical bones activated for skeleton: ", skel)
 	else:
 		print("Skeleton2D does not support physical bones!")
-	
-	# skel.physical_bone_chain_length(2)
-
-	# Recursively print all PhysicalBone2D nodes and their CollisionShape2D children
 	_recursive_print_bones(skel)
 
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	move_and_slide()
 
 func _recursive_print_bones(node):
 	if node is PhysicalBone2D:
