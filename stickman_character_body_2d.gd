fd95d583f6976f163f189c31a59bd59a147a8cb4
70ziko <fidler.patryk@gmail.com>
2025-06-02 10:46:57 +0200

    refactor: reorganize and streamline physics processing by dividing into atomic functions

--- stickman_character_body_2d.gd
+++ stickman_character_body_2d.gd
@@ -4,15 +4,6 @@ var gravity: float = float(ProjectSettings.get_setting("physics/2d/default_gravi
 var speed := 200.0
 var movement_direction := 0 # -1 for left, 1 for right, 0 for idle
 
-func _recursive_print_bones(node):
-	if node is PhysicalBone2D:
-		print("Found PhysicalBone2D: ", node.get_path())
-		for child in node.get_children():
-			if child is CollisionShape2D:
-				print("  Has CollisionShape2D: ", child.shape)
-	for child in node.get_children():
-		_recursive_print_bones(child)
-
 func _ready():
 	var skel = $Skeleton2D
 	print("Godot version:", Engine.get_version_info())
@@ -24,6 +15,24 @@ func _ready():
 	_recursive_print_bones(skel)
 
 func _physics_process(delta):
+	_direction_from_keyboard()
+	
+	velocity.x = movement_direction * speed
+	_ground_collision(delta)
+
+	move_and_slide()
+
+
+func _recursive_print_bones(node):
+	if node is PhysicalBone2D:
+		print("Found PhysicalBone2D: ", node.get_path())
+		for child in node.get_children():
+			if child is CollisionShape2D:
+				print("  Has CollisionShape2D: ", child.shape)
+	for child in node.get_children():
+		_recursive_print_bones(child)
+
+func _direction_from_keyboard():
 	var input_dir = 0
 	if Input.is_action_pressed("ui_left"):
 		input_dir -= 1
@@ -33,11 +42,8 @@ func _physics_process(delta):
 	if movement_direction != input_dir:
 		movement_direction = input_dir
 		
-	velocity.x = movement_direction * speed
-
+func _ground_collision(delta):
 	if position.y <= 490:
 		velocity.y += gravity * delta
 	else:
 		velocity.y = 0
-
-	move_and_slide()
